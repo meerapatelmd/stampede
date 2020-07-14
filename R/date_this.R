d7858dc1a48c8f6d8189db58d60efda3b88a5b57
Meera Patel <patelm9@mskcc.org>
2020-07-13 21:07:24 -0400

    add date_this function

--- R/date_this.R
+++ R/date_this.R
@@ -2,8 +2,8 @@
 #' @param without_punct default is FALSE. If true, returns date in YYYYmmdd format. Otherwise YYYY-mm-dd format.
 #'@export
 
-dated <- function(punct = FALSE) {
-        if (punct == FALSE) {
+date_this <- function(without_punct = FALSE) {
+        if (without_punct == FALSE) {
                 return(as.character(Sys.Date()))
         } else {
                 return(as.character(format(Sys.Date(), "%Y%m%d")))
